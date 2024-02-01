1c65c42315ab4ea77ea33dbbb8ac60ed14819cd2
Kritika Farswan <128362461+Kzrqirtf@users.noreply.github.com>
2024-02-01 12:00:37 +0530

    Time: 3 ms (86.45%) | Memory: 23.1 MB (15.85%) - LeetSync

---
+++ Strings/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseWords(string s)
+    {
+        stringstream ss(s);
+        string words;
+        string ans;
+        while (ss >> words) {   
+            if (ans != "")  
+                ans = words + " " + ans;
+            else   
+                ans = words + ans;
+        }
+        return ans;
+    }
+};
