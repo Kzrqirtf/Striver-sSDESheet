0a45bcd4510045489d84af230239b96009b805bb
Kritika Farswan <128362461+Kzrqirtf@users.noreply.github.com>
2024-02-03 11:54:46 +0530

    Time: 22 ms (44.11%) | Memory: 24.7 MB (17.20%) - LeetSync

---
+++ Binary-Search/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        int start = 0;
+        int end = n - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            // Check if mid is the single non-duplicate element
+            if ((mid == 0 || nums[mid] != nums[mid - 1]) && (mid == n - 1 || nums[mid] != nums[mid + 1])) {
+                return nums[mid];
+            }
+
+            // Adjust start and end based on conditions
+            if (mid % 2 == 0) {
+                if (mid < n - 1 && nums[mid] == nums[mid + 1]) {
+                    start = mid + 2;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if (mid > 0 && nums[mid] == nums[mid - 1]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
