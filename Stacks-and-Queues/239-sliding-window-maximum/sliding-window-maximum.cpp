b9a7def148ba35a7a8a64dc0f14ee684ac6ca5cd
Kritika Farswan <128362461+Kzrqirtf@users.noreply.github.com>
2024-02-11 22:15:29 +0530

    Time: 170 ms (88.95%) | Memory: 137 MB (49.30%) - LeetSync

--- Stacks-and-Queues/239-sliding-window-maximum/sliding-window-maximum.cpp
+++ Stacks-and-Queues/239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -14,19 +14,31 @@ public:
         //     end++;
         // }
         // return ans;
-    deque <int> dq;
-    vector<int> ans;
-    for(int i =0 ;i<nums.size();i++)
-    {
-        if(!dq.empty() && dq.front() == i-k)
-            dq.pop_front();
-        while(!dq.empty() && nums[dq.back()] < nums[i])
-            dq.pop_back();
-        dq.push_back(i);
-        if(i >= k-1)
-            ans.push_back(nums[dq.front()]);
-    }
-    return ans;
+    
+        // Initialize a deque and a vector to store the maximum values and return at the end
+        deque<int> dq;
+        vector<int> ans;
         
+        // Iterate through the input vector 'nums'
+        for(int i = 0; i < nums.size(); i++) {
+            // If the deque is not empty and the front element is out of the window range, remove it
+            if(!dq.empty() && dq.front() == i - k)
+                dq.pop_front();
+            
+            // Remove all elements from the back of the deque which are smaller than the current element
+            while(!dq.empty() && nums[dq.back()] < nums[i])
+                dq.pop_back();
+            
+            // Push the current element's index into the deque
+            dq.push_back(i);
+            
+            // If the current index has reached the window size or greater, 
+            // it means we have a full window and we can start recording maximum values
+            if(i >= k - 1)
+                ans.push_back(nums[dq.front()]);
+        }
+        
+        // Return the vector containing maximum values for each sliding window
+        return ans;
     }
 };
